3e43328e4bfc257e427eb990e169b556acafd500
Lucas Gabriel <163341948+Dev-Lucas-Ferreira@users.noreply.github.com>
2025-04-23 21:11:33 +0000

    fiz o pulo e a gravidade

--- Mario-Run/js/script.js
+++ Mario-Run/js/script.js
@@ -1,28 +1,59 @@
 //variaveis do jogador
 const player = document.getElementById('character');
-let player_speed = 100;
+let player_speed = 13;
+
 
 //eixos
-let positionX = 0;
+let positionX = 0;//E o chão e faz parte da gravidade
 let positionY = 0;
 
+//gravidade
+let speedY = 0;
+let gravity = 0.5;
+let xrl8 = 0;
+let deltaTime = 0.2;
+
+function loadphysic(){
+    const container = document.querySelector('.container');
+    const ground = container.clientHeight - player.offsetHeight;
+    //atualizando a velocidade e a posição Y
+    speedY += gravity*deltaTime;
+    positionY += speedY*deltaTime;
+
+    if(positionY >= ground){
+        positionY = ground;
+        speedY = 0;
+    }
+    player.style.top = `${positionY}px`;
+    console.log(`posiçãoY:${positionY}, SpeedY:${speedY}`);
+}
+
 //Função de movimento w,a,s,d FUNCIONANDO!!
-document.addEventListener('keydown', function (event) {
-    let sprites = "";
+document.addEventListener('keydown', function(event){
+
+    //variaveis do evento
     const key = event.key;
     let cenarios = "";
     const img = document.getElementById('character')
 
-    if (key === 'w' || key === 'W') {
-        positionY -= player_speed;  
-    } else if (key === 'a' || key === 'A') {
+    //teclas
+    if (key === ' ' || key === 'w' || key === 'W') {
+        const container = document.querySelector('.container');
+        const ground = container.clientHeight - player.offsetHeight;
+        if(positionY >= ground){
+            speedY = -15;
+        }
+    } 
+    if (key === 'a' || key === 'A') {
         positionX -= player_speed;
-
+        player.src = "/Mario-Run/img/esquerda.png"
     } else if (key === 's' || key === 'S') {
         positionY += player_speed;
     } else if (key === 'd' || key === 'D') {
         positionX += player_speed;
+        player.src = "/Mario-Run/img/direita.png"
     }
+
     //Cenarios
     if (key === '1') {
         cenarios = "url(/Mario-Run/img/cenario/cenario-temporariio.avif)"
@@ -38,15 +69,15 @@ document.addEventListener('keydown', function (event) {
 
 
     parede();
-
-
-    //fiz o chat fazer essa para pegar as cordenadas "alterar depois pois pode bugar"
+    //O chat fez para pegas a posição do player
     const rect = player.getBoundingClientRect();
     console.log(`Posição X: ${rect.x}, Posição Y: ${rect.y}`);
 })
 
 //demorei mas consegui fazer a maior parte sozinho ultilizei o chat para me explicar as funções e me dizer os erros 
 //re-fiz essa merda mais de 23 vezes HAAAAAAAAAAAAAAAAAAAAAA :)
+
+//colisão
 function parede() {
     //altura e largura do player e do container
     const container = document.querySelector('.container');
@@ -70,7 +101,7 @@ function parede() {
     player.style.left = `${positionX}px`;
     player.style.top = `${positionY}px`;
 }
-
+//decide onde o personagem vai aparecer
 function spawn_do_personagem() {
     const spawX = window.innerWidth;
     const spawY = window.innerHeight;
@@ -82,3 +113,7 @@ function spawn_do_personagem() {
     player.style.top = `${positionY}px`;
 }
 window.onload = spawn_do_personagem;
+setInterval(loadphysic);
+
+
+
